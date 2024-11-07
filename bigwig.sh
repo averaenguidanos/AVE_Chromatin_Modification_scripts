0e8e882c80df25b18def637673b218c762a80718
RdeACdeP <ricardo60300@gmail.com>
2024-11-07 13:07:03 +0100

    Removing old scripts

--- bigwig.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-bamCoverage -b h3.bam --normalizeUsing RPKM -o h3.bw
-bamCoverage -b h3k36me1.bam --normalizeUsing RPKM -o h3k36me1.bw
-bamCoverage -b h3k36me2.bam --normalizeUsing RPKM -o h3k36me2.bw
-bamCoverage -b h3k36me3.bam --normalizeUsing RPKM -o h3k36me3.bw
-bamCoverage -b h3k37me1.bam --normalizeUsing RPKM -o h3k37me1.bw
--- peaks.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-epic2 --treatment h3k36me1.bam --control h3.bam --chromsizes chromsizes > h3k36me1_over_h3.peaks.bed
-epic2 --treatment h3k36me3.bam --control h3.bam --chromsizes chromsizes > h3k36me3_over_h3.peaks.bed
-epic2 --treatment h3k37me1.bam --control h3.bam --chromsizes chromsizes > h3k37me1_over_h3.peaks.bed
