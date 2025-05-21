b4cbfd490c249930e11193461a8f41dcdbd95a82
averaenguidanos <averaenguidanos@gmail.com>
2025-05-21 10:28:10 +0200

    Update chromHMM.sh

--- chromHMM.sh
+++ chromHMM.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 
-
 ############################# LOADING PARAMETERS #############################
 
 # Working directory: absolute path to the folder containing all files.
@@ -11,17 +10,15 @@ GENOME=$(grep genome_path: $1 | awk '{ print $2 }')
 # Number of processors.
 THREADS=$(grep number_processors: $1 | awk '{ print $2 }')
 
+
+
+## A. Calculating coverage over the designated bins for later analysis ##
+
 ############################# GETTING GENOMIC BINS #############################
 
 cd $GENOME
 
-mysql --user=genome --host=genome-mysql.cse.ucsc.edu -A -e 'select chrom, size from hg38.chromInfo' > hg38.pre_genome
-
-grep -v 'size' hg38.pre_genome > hg38.genome
-
-rm hg38.pre_genome
-
-bedtools makewindows -g hg38.genome -w 50000 -s 50000 > genome_50kb.bed
+bedtools makewindows -g hg38.genome -w 50000 > genome_50kb.bed
 
 ############################# COVERAGE OVER REGIONS #############################
 
@@ -30,6 +27,11 @@ cd ${WD}/results
 multiBamSummary BED-file -p 40 --BED /home/gmzlab/Documents/ricardo/genome/homo_sapiens_unknown/genome_50kb.bed -b ../bam_files/*.bam -o results.npz --outRawCounts output_rawCount.txt
 
 cat output_rawCount.txt | sort -k1,1 -k2,2n > output_rawCount.sort.txt
+
+
+
+
+## B. Preparing and training the HMM model for chromatin states
 
 ############################# CONVERTING BAMS TO BEDS #############################
 
@@ -47,21 +49,18 @@ java -jar /home/gmzlab/opt/ChromHMM/ChromHMM.jar BinarizeBed -b 50000 /home/gmzl
 
 ############################# CHROMATIN STATES #############################
 
-java -jar /home/gmzlab/opt/ChromHMM/ChromHMM.jar LearnModel -p 40 -b 50000 results/binarize results/model 4 hg38
+# in results/chromHMM
+java -jar ../../opt/ChromHMM/ChromHMM.jar LearnModel -p 12 -b 50000 binarize_agustin_50kb/ model_50kb 4 hg38 
 
 #############################  #############################
 
 
 
+## C. Obtain the correlation between the emission parameters of differente models and a reference ##
 
+############################# CORRELATION BETWEEN MODELS #############################
 
+java -jar ../../opt/ChromHMM/ChromHMM.jar CompareModels
 
-
-
-
-
-
-
-
-
+#############################  #############################
 
