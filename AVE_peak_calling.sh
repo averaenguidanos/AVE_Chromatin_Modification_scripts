8851bd7b47055765e67fa292acc25af04d3fce6b
averaenguidanos <averaenguidanos@gmail.com>
2025-07-04 09:52:23 +0200

    Update and rename peak_calling.sh to AVE_peak_calling.sh

--- AVE_peak_calling.sh
+++ AVE_peak_calling.sh
@@ -24,7 +24,7 @@ then
 if [ $INPUT != 'not_provided' ] && [ $HISTONE != 'not_provided' ]
 then
 
-# Calling peaks on both input and H3 but separately
+# Calling peaks on both input and histones but separately
 epic2 --guess-bampe --treatment ${WD}/samples/${CHIP}/${CHIP}*.bam --control ${WD}/samples/${INPUT}/${INPUT}*.bam --chromsizes ${GENOME}/chromsizes > ${CHIP}_over_${INPUT}.peaks.bed
 
 epic2 --guess-bampe --treatment ${WD}/samples/${CHIP}/${CHIP}*.bam --control ${WD}/samples/${HISTONE}/${HISTONE}*.bam --chromsizes ${GENOME}/chromsizes > ${CHIP}_over_${HISTONE}.peaks.bed
@@ -61,10 +61,6 @@ else
 if [ $INPUT != 'not_provided' ] && [ $HISTONE != 'not_provided' ]
 then
 
-# Calling peaks on both input and H3 but separately
-epic2 --treatment ${WD}/samples/${CHIP}/${CHIP}_1.bam ${WD}/samples/${CHIP}/${CHIP}_2.bam --control ${WD}/samples/${INPUT}/${INPUT}_1.bam --chromsizes ${GENOME}/chromsizes > ${CHIP}_over_${INPUT}.peaks.bed
-
-epic2 --treatment ${WD}/samples/${CHIP}/${CHIP}_1.bam ${WD}/samples/${CHIP}/${CHIP}_2.bam --control ${WD}/samples/${HISTONE}/${HISTONE}_1.bam --chromsizes ${GENOME}/chromsizes > ${CHIP}_over_${HISTONE}.peaks.bed
 
 # Extracting high confidence sets by intersection of both peak calling modes (vs input and histone)
 # -u: (unique) if any overlap exists, the A feature is reported.
@@ -100,14 +96,3 @@ fi
 bedtools merge -d ${MAX_DIST} -i ${CHIP}.peaks.bed > ${CHIP}.regions.bed
 
 # When analysing ChIP-seq data on human cells MAX_DIST was set to 5000 bp and when analysing data on yeast cells, it was set to 1000 bp.
-
-# EXTRA LUCA
-
-# -n: numeric data.
-# -k: number of column to sort on.
-# -r: reverse order.
-#bedtools merge -d 5000 -i ${CHIP}.peaks.bed | sort -n -k 7 -r - | cut -f1,2,3 > ${CHIP}.regions.bed
-
-# Quick and dirty strategy to create a linkable table of regions
-#bedtools slop -b 0.25 -pct -i ${CHIP_GENERAL}.regions.bed -g ${WORK_DIR}/${MAIN_FOLDER}/genome/genome.chrom.sizes | bedtools links -base 'https://genome-euro.ucsc.edu' -db 'hg38&hgS_doOtherUser=submit&hgS_otherUserName=LucaPandol$
-
